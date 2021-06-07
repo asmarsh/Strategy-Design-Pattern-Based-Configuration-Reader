09a94d57bddb51179211deef6defedf81e90f066
Andrew Marsh <21085039+asmarsh@users.noreply.github.com>
2021-06-06 21:57:46 -0400

    Adding hacky unit tests with arrange-act-assert format to satisfy assignment requirements

--- Strategy.Tests/StrategyTests.cs
+++ Strategy.Tests/StrategyTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Strategy.Tests
@@ -24,6 +25,43 @@ namespace Strategy.Tests
 
             Assert.IsNotNull(result);
             Assert.AreEqual("testResultTwo", result);
+        }
+
+        [TestMethod]
+        public void CheckConfigurationReaderReadsSuppliedKeyValue()
+        {
+            // Arrange
+            var keyName = "thisIsAKey";
+            var value = "value";
+            ConfigurationManager.AppSettings[keyName] = value;
+
+            // Act
+            var reader = new ConfigurationReader();
+            var result = reader.Read(keyName);
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(value, result);
+        }
+
+        [TestMethod]
+        public void CheckConnectionReaderReadsSuppliedKeyValue()
+        {
+            // Arrange
+            var keyName = "thisIsAKey";
+            var value = "value";
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.ConnectionStrings.ConnectionStrings.Add(new ConnectionStringSettings(keyName, value));
+            config.Save(ConfigurationSaveMode.Modified, true);
+            ConfigurationManager.RefreshSection("connectionStrings");
+
+            // Act
+            var reader = new ConnectionReader();
+            var result = reader.Read(keyName);
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(value, result);
         }
     }
 }
